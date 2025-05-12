54337f9ca7483d04bee35a9ba72e1538e24cc4fe
yasinarafat4 <mdyasinarafathasib@gmail.com>
2025-05-12 23:32:57 +0600

    Database Connected and User Table Created

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ const db = await mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  //   database: 'test',
+  database: "express_crud_db",
 });
 console.log("MySQL Connected Successfully!");
+// SQL to create a table
+const createTableQuery = `
+      CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL UNIQUE,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `;
+
+// Execute the query
+await db.execute(createTableQuery);
+console.log("'users' table created Successfully!");
