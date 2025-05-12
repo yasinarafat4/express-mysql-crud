b18bab7bdd0beed9868ec9349032190c991eed6e
yasinarafat4 <mdyasinarafathasib@gmail.com>
2025-05-12 23:59:27 +0600

    Insert Data Done

--- app.js
+++ app.js
@@ -9,16 +9,28 @@ const db = await mysql.createConnection({
   database: "express_crud_db",
 });
 console.log("MySQL Connected Successfully!");
-// SQL to create a table
-const createTableQuery = `
-      CREATE TABLE IF NOT EXISTS users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(100) NOT NULL UNIQUE,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      )
-    `;
+
+// SQL create a table
+// const createTableQuery = `
+//       CREATE TABLE IF NOT EXISTS users (
+//         id INT AUTO_INCREMENT PRIMARY KEY,
+//         name VARCHAR(100) NOT NULL,
+//         email VARCHAR(100) NOT NULL UNIQUE,
+//         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+//       )
+//     `;
 
 // Execute the query
-await db.execute(createTableQuery);
-console.log("'users' table created Successfully!");
+// await db.execute(createTableQuery);
+// console.log("'users' table created Successfully!");
+
+// CRUD Operations
+// Insert:
+try {
+  await db.execute(`
+    INSERT INTO users(name, email) VALUES('Ambia', 'ambia@gmail.com')
+  `);
+  console.log("User inserted successfully!");
+} catch (error) {
+  console.error("Insert failed:", error.message);
+}
